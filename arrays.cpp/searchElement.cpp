351fa4001906c125504c0cc238cc9e8ec5851961
Renu Singh <renusinghch2022@gmail.com>
2025-08-06 14:29:52 +0530

    Search and element and print its location

--- arrays.cpp/searchElement.cpp
+++ arrays.cpp/searchElement.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int searchElement(int arr[], int n, int x) {
+int searchElement(int arr[], int n,int x) {
     for(int i=0;i<n;i++) {
         if(arr[i] == x) {
             return i;
@@ -12,7 +12,9 @@ int main()
 {
     int arr[] ={1,2,34,5,6,7,8,9,29,64};
     int x=64;
+
     int location = searchElement(arr,10,x);
     cout<<"Element found at location = "<<location;
     return 0;
 }
+
