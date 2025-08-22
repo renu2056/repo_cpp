c3e835b884b0694ed595ccd5d61324d34bcf34ce
Renu Singh <renusinghch2022@gmail.com>
2025-08-22 15:35:03 +0530

    simple structure of classes and objects

--- classes/objects_practice.cpp
+++ classes/objects_practice.cpp
@@ -1,3 +1,6 @@
+//create simple class with two data members:
+//create objects and initialize
+
 #include<iostream>
 #include<string.h>
 using namespace std;
@@ -17,5 +20,34 @@ int main() {
     s1.name = "Renu";
     s1.age = 19;
     cout<<s1.name<<endl;
+    return 0;
+}
+
+// make data members private and use public functions:
+//final main fucntions:
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    void isStudent(string new_name, int a) {
+        name = new_name;
+        age = a;
+    }
+    void display() {
+        cout<<"Name: "<<name<<endl<<"Age: "<<age<<endl;
+    }
+};
+
+int main() {
+    Student s1;
+    s1.isStudent("Renu",19);
+    s1.display();
     return 0;
 }
